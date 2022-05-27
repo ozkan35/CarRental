23eb9c3017707bb222dbe11e63952f1935124558
unknown <ozkanust35@gmail.com>
2022-05-27 22:28:45 +0300

    010

---
+++ src/api/admin-user-service.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+const API_URL = process.env.REACT_APP_API_URL;
+const getUsers = () => {
+  return axios.post(`${API_URL}/user/auth/all`, { headers: authHeader() });
+};
+export { getUsers }
